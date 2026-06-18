dfce60d13f9751703343a60b15cbaacb13c69763
Ux7umid7 <ux7umidjon7@gmail.com>
2026-06-18 09:50:06 +0500

    Add src/services/api.ts

---
+++ src/services/api.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { useAuthStore } from '@store/authStore';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Request interceptor
+api.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().token;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// Response interceptor
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      useAuthStore.getState().logout();
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
